785eb674cf0e97323640bb5b06a3b462725c64c6
shilpi <shilpiguptabca@gmail.com>
2024-03-29 01:02:49 +0530

    djangofashion

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('assets')]
+STATICFILES_DIRS = [BASE_DIR.joinpath('/static')]
 STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
 
 MEDIA_URL = '/media/'
